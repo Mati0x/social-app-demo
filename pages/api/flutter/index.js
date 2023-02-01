88da7425bcffaf75dc3d458ebf0626fa99d27735
Matias <mantitaaa89@gmail.com>
2023-02-01 16:15:06 -0300

    refactor: add the follwing: -new endpoints

--- pages/api/flutter/index.js
+++ pages/api/flutter/index.js
@@ -1,17 +1,31 @@
 export default async function handler(req, res) {
   const fetchOptions = {
-    method: "",
-    headers: {},
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.MONGODB_DATA_API_KEY,
+    },
   };
-  const fetchBody = {};
+  const fetchBody = {
+    dataSource: process.env.MONGODB_DATA_SOURCE,
+    database: "social_butterfly",
+    collection: "flutters",
+  };
   const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
 
   try {
     switch (req.method) {
       case "GET":
-        const readData = await fetch('<url>', '<options>');
+        const readData = await fetch(`${baseUrl}/find`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            sort: { postedAt: -1 },
+          }),
+        });
         const readDataJson = await readData.json();
-        res.status(  ).json(  );
+        res.status(200).json(readDataJson.documents);
         break;
       default:
         res.status(405).end();
